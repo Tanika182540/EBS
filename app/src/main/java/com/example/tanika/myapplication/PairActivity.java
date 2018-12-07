dfd6e47d69603bbcafc18e5c12e7ede228320cda
Nopphon <nopphonyel@gmail.com>
2018-12-07 21:25:39 +0700

    Adding pair activity

---
+++ app/src/main/java/com/example/tanika/myapplication/PairActivity.java
@@ -0,0 +1,13 @@
+package com.example.tanika.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PairActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pair);
+    }
+}
